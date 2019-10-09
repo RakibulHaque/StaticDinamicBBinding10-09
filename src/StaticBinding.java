75a70e6ef121b32676455e86bc55f2c4e2af750d
RakibulHaque <hrakibul@sun21.co.jp>
2019-10-09 17:38:35 +0900

    commit

---
+++ src/StaticBinding.java
@@ -0,0 +1,30 @@
+
+public class StaticBinding {
+
+	public static class superclass
+	{
+		static void print() {
+			System.out.println("print in superclass");
+			
+		}
+		
+	}
+	public static class subclass extends superclass
+	{
+		static void print()
+		{
+			System.out.println("print in subclass");
+			
+		}
+	}
+	public static void main(String[] args)
+	{
+		superclass A = new superclass();
+		subclass B = new subclass();
+		A.print();
+		B.print();
+	}
+	
+	
+	
+}
